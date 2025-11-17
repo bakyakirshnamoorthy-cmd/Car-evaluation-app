f665a97778e11e574e7d9dec527c19bbf522e594
bakyakirshnamoorthy-cmd <bakyakirshnamoorthy@gmail.com>
2025-11-18 04:24:08 +0530

    Update and rename app_py.py to app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1U8AqTSGCWljdYBlFEeyHqkBGqglSrKMR
 """
 
-!pip install streamlit
 !pip install pandas
 !pip install numpy
 !pip install joblib
